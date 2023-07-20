219591c39777169ab1bd11f128f1e5f6e7adc883
Lydia Scharron <lfscharr@gmail.com>
2023-07-20 19:49:26 -0400

    ph0p3introtofunc

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  // shout('hello') 
+
+function whisper(string) {
+    return string.toLowerCase()
+} 
+
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+// logShout('hello')
+
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+// logwhisper('hello')
+
+function sayHiToHeadphonedRoommate(string) {
+    if(string === string.toLowerCase()){
+    return "I can't hear you!"
+    } 
+    if(string === string.toUpperCase()){
+return "YES INDEED!"
+    }
+if(string === "Let's have dinner together!"){
+return "I would love to!"
+}
+
+}
+
+    
+
